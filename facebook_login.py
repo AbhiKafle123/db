bd2598b3f912f651c679f8d2ced2a60bf98b3d93
Abhikafle123 <067bct501@ioe.edu.np>
2016-10-29 16:18:24 -0500

    Added a sample file to include everything about a user according to ERD

---
+++ facebook_login.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#Asks for permission and prints name/id
+
+from flask import Flask, redirect, url_for, session, request
+from flask_oauth import OAuth
+
+
+SECRET_KEY = 'development key'
+DEBUG = True
+FACEBOOK_APP_ID = '1664088927192847'
+FACEBOOK_APP_SECRET = '745acb1c7b53c9e27babd3d9f15a852d'
+
+
+app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
+
+if __name__ == '__main__':
+    app.run()
--- pull_data.py
+++ pull_data.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python
+#Pulling data from graph api in a single JSON file
+# Name,ID followed by Likes followed by tagged_places followed by friends
+
+
 import os
 
 access_token = raw_input()
+print os.system('curl -sS \'https://graph.facebook.com/v2.8/me?limit=1000&access_token={}\' >> places.json'.format(access_token) )
 
-print os.system('curl -sS \'https://graph.facebook.com/v2.8/me/likes?limit=1000&access_token={}\' >> likes.json'.format(access_token) )
+print os.system('curl -sS \'https://graph.facebook.com/v2.8/me/likes?limit=1000&fields=category,name&access_token={}\' >> places.json'.format(access_token) )
+
 print os.system('curl -sS \'https://graph.facebook.com/v2.8/me/tagged_places?limit=500&access_token={}\' >> places.json'.format(access_token) )
 
+print os.system('curl -sS \'https://graph.facebook.com/v2.8/me/friends?limit=1000&access_token={}\' >> places.json'.format(access_token) )
