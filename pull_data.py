fb53a92fc59be900adb433e08f9bc9880706b1fc
Abhikafle123 <067bct501@ioe.edu.np>
2016-10-29 22:13:20 -0500

    Added pagination support

--- pull_data.py
+++ pull_data.py
@@ -4,12 +4,27 @@
 
 
 import os
-
+import requests
+import json
 access_token = raw_input()
-print os.system('curl -sS \'https://graph.facebook.com/v2.8/me?limit=1000&access_token={}\' >> places.json'.format(access_token) )
 
-print os.system('curl -sS \'https://graph.facebook.com/v2.8/me/likes?limit=1000&fields=category,name&access_token={}\' >> places.json'.format(access_token) )
 
-print os.system('curl -sS \'https://graph.facebook.com/v2.8/me/tagged_places?limit=500&access_token={}\' >> places.json'.format(access_token) )
+# response = os.system('wget "https://graph.facebook.com/v2.8/me?fields=id,name,likes{category,name,fan_count},friends,tagged_places&access_token=%s" -O all3.json'%(access_token) )
+#response = requests.get('https://graph.facebook.com/v2.8/me?fields=id,name,likes{category,name,fan_count},friends,tagged_places&access_token=%s' % (access_token))
+response = requests.get('https://graph.facebook.com/v2.8/me/likes?fields=category,name&limit=100&access_token=%s' % (access_token)).json()
 
-print os.system('curl -sS \'https://graph.facebook.com/v2.8/me/friends?limit=1000&access_token={}\' >> places.json'.format(access_token) )
+# print response['paging']['next']
+likes = response['data']
+count = 0
+while 'paging' in response:
+#	print count
+#	count += 1
+	nexturl = response['paging']['next']
+	response = requests.get(nexturl).json()
+	likes.extend(response['data'])
+print likes
+#print sum([1 for l in likes])
+
+
+
+
