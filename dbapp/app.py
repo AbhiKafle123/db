7672f8f0bb8ee229dc65c59ead2f46c9ea476db2
SurajSubedi <srt983@gmail.com>
2016-11-05 15:51:37 -0500

    Login page linked to fb authorization page.

--- dbapp/app.py
+++ dbapp/app.py
@@ -1,22 +1,28 @@
-from flask import Flask, render_template, json, request
+#! /usr/bin/python
+from flask import Flask, redirect, url_for, session, request, render_template
+from flask_oauth import OAuth
+
+
+SECRET_KEY = 'development key'
+DEBUG = True
+FACEBOOK_APP_ID = '1664088927192847'
+FACEBOOK_APP_SECRET = '745acb1c7b53c9e27babd3d9f15a852d'
+
 
 app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
 
-# mysql = MySQL()
-# My SQL configurations
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-# app.config['MYSQL_DATABASE_DB'] = 'BucketList'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
-
-#create mysql connection
-# conn = mysql.connect()
-# cursor = conn.cursor()
-
-def getJsonData():
-    jsondata = json.load(open("data/data.json"))
-    return jsondata 
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'user_friends, user_likes, user_location, user_tagged_places, user_photos'}
+)
 
 @app.route("/")
 def main():
@@ -24,26 +30,38 @@ def main():
 	# print (type(jsondata))
 	return render_template("login.html",template_folder='templates')
 
+
 @app.route('/signup')
-def showSignUp():
-	return render_template('index.html')
+def index():
+    return redirect(url_for('login'))
 
-@app.route('/ShowsignUp', methods=['POST'])
-def signUp():
-	# read the posted values from the UI
-	_name = request.form['inputName']
-	_email = request.form['inputEmail']
-	_password = request.form['inputPassword']
 
-	# validate the received values
-	if _name and _email and _password:
-		_hashed_password = generate_password_hash(_password)
-		cursor.callproc('sp_createUSer', (_name, _email, _hashed_password))
-		return json.dumps({'html': '<span>All fields good </span>'})
-	else:
-		return json.dumps({'html': '<span>Enter the required fields'})
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
 
-if __name__ == "__main__":
-	app.run()
 
-# 
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s session token=%s' % \
+        (me.data['id'], me.data['name'], resp['access_token'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+    print session.get('oauth_token')
+
+
+if __name__ == '__main__':
+    app.run()
